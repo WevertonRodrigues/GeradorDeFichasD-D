f06a7e6a9f0b7c0fea5cd5f542d154e87b98a4cd
WevertonRodrigues <weverton.rodrigues96@yahoo.com>
2020-02-07 20:29:12 -0300

    Alterações funcionais modal magias

--- src/app/components/ficha/modal-magias/modal-magias.component.ts
+++ src/app/components/ficha/modal-magias/modal-magias.component.ts
@@ -15,17 +15,116 @@ export class ModalMagiasComponent implements OnInit, AfterViewInit {
 
   @Output() private salvarAlteracoes = new EventEmitter();
 
-  @ViewChild('magia1', { static: false }) magia1: HTMLElement;
-  @ViewChild('magia2', { static: false }) magia2: HTMLElement;
-
+  @ViewChild('amizade', { static: false }) amizade: HTMLElement;
+  @ViewChild('ataqueCerteiro', { static: false }) ataqueCerteiro: HTMLElement;
+  @ViewChild('bordaoMistico', { static: false }) bordaoMistico: HTMLElement;
+  @ViewChild('chamaSagrada', { static: false }) chamaSagrada: HTMLElement;
+  @ViewChild('consertar', { static: false }) consertar: HTMLElement;
+  @ViewChild('chicoteDeEspinhos', { static: false }) chicoteDeEspinhos: HTMLElement;
+  @ViewChild('criarChamas', { static: false }) criarChamas: HTMLElement;
+  @ViewChild('druidismo', { static: false }) druidismo: HTMLElement;
+  @ViewChild('espirroAcido', { static: false }) espirroAcido: HTMLElement;
+  @ViewChild('estabilizar', { static: false }) estabilizar: HTMLElement;
+  @ViewChild('globosDeLuz', { static: false }) globosDeLuz: HTMLElement;
+  @ViewChild('orientacao', { static: false }) orientacao: HTMLElement;
+  @ViewChild('luz', { static: false }) luz: HTMLElement;
+  @ViewChild('ilusaoMenor', { static: false }) ilusaoMenor: HTMLElement;
+  @ViewChild('maosMagicas', { static: false }) maosMagicas: HTMLElement;
+  @ViewChild('mensagem', { static: false }) mensagem: HTMLElement;
+  @ViewChild('prestidigitacao', { static: false }) prestidigitacao: HTMLElement;
+  @ViewChild('protecaoContraLaminas', { static: false }) protecaoContraLaminas: HTMLElement;
+  @ViewChild('raioDeFogo', { static: false }) raioDeFogo: HTMLElement;
+  @ViewChild('raioDeGelo', { static: false }) raioDeGelo: HTMLElement;
+  @ViewChild('rajadaDeVeneno', { static: false }) rajadaDeVeneno: HTMLElement;
+  @ViewChild('resistencia', { static: false }) resistencia: HTMLElement;
+  @ViewChild('rajadaMistica', { static: false }) rajadaMistica: HTMLElement;
+  @ViewChild('taumaturgia', { static: false }) taumaturgia: HTMLElement;
+  @ViewChild('toqueArrepiante', { static: false }) toqueArrepiante: HTMLElement;
+  @ViewChild('toqueChocante', { static: false }) toqueChocante: HTMLElement;
+  @ViewChild('zombariaViciosa', { static: false }) zombariaViciosa: HTMLElement;
 
   inputs: { elemento: HTMLElement, value: string }[] = [{
-    elemento: this.magia1, value: 'magia1'
+    elemento: this.amizade, value: 'amizade'
   },
   {
-    elemento: this.magia2, value: 'magia2'
-  }]
-
+    elemento: this.ataqueCerteiro, value: 'ataqueCerteiro'
+  },
+  {
+    elemento: this.bordaoMistico, value: 'bordaoMistico'
+  },
+  {
+    elemento: this.chamaSagrada, value: 'chamaSagrada'
+  },
+  {
+    elemento: this.consertar, value: 'consertar'
+  },
+  {
+    elemento: this.chicoteDeEspinhos, value: 'chicoteDeEspinhos'
+  },
+  {
+    elemento: this.criarChamas, value: 'criarChamas'
+  },
+  {
+    elemento: this.druidismo, value: 'druidismo'
+  },
+  {
+    elemento: this.espirroAcido, value: 'espirroAcido'
+  },
+  {
+    elemento: this.estabilizar, value: 'estabilizar'
+  },
+  {
+    elemento: this.globosDeLuz, value: 'globosDeLuz'
+  },
+  {
+    elemento: this.orientacao, value: 'orientacao'
+  },
+  {
+    elemento: this.luz, value: 'luz'
+  },
+  {
+    elemento: this.ilusaoMenor, value: 'ilusaoMenor'
+  },
+  {
+    elemento: this.maosMagicas, value: 'maosMagicas'
+  },
+  {
+    elemento: this.mensagem, value: 'mensagem'
+  },
+  {
+    elemento: this.prestidigitacao, value: 'prestidigitacao'
+  },
+  {
+    elemento: this.protecaoContraLaminas, value: 'protecaoContraLaminas'
+  },
+  {
+    elemento: this.raioDeFogo, value: 'raioDeFogo'
+  },
+  {
+    elemento: this.raioDeGelo, value: 'raioDeGelo'
+  },
+  {
+    elemento: this.rajadaDeVeneno, value: 'rajadaDeVeneno'
+  },
+  {
+    elemento: this.resistencia, value: 'resistencia'
+  },
+  {
+    elemento: this.rajadaMistica, value: 'rajadaMistica'
+  },
+  {
+    elemento: this.taumaturgia, value: 'taumaturgia'
+  },
+  {
+    elemento: this.toqueArrepiante, value: 'toqueArrepiante'
+  },
+  {
+    elemento: this.toqueChocante, value: 'toqueChocante'
+  },
+  {
+    elemento: this.zombariaViciosa, value: 'zombariaViciosa'
+  },
+  ]
 
   magias;
 
@@ -39,14 +138,14 @@ export class ModalMagiasComponent implements OnInit, AfterViewInit {
 
   adicionarMagia(magia: string, nivel: number) {
     //console.log( this.magias[nivel].includes( { NomeMagia : magia } ) )
-    let i : number = this.magias[nivel].findIndex(val => val.NomeMagia === magia)
+    let i: number = this.magias[nivel].findIndex(val => val.NomeMagia === magia)
 
     if (i < 0)
       this.magias[nivel].push({ NomeMagia: magia })
     else
       this.magias[nivel].splice(i, 1)
 
-    
+
   }
 
   salvarMagias() {
@@ -62,23 +161,31 @@ export class ModalMagiasComponent implements OnInit, AfterViewInit {
       )
     this.salvarAlteracoes.emit({ Magias: this.magias, i: this.index })
   }
-  fechar() {
-    console.log(this.magias)
-    console.log(this.magiasPai)
+
+  restaurarMagias() {
+    /*console.log(this.magias)
+    console.log(this.magiasPai)*/
     if (this.magias.length > 0)
-      if (confirm("Fechar e perder alterações?"))
+      if (confirm("Apagar todas as magias?")) {
+        this.magias = this.criarMatriz();
+
+      }
+    this.salvarAlteracoes.emit({ Magias: this.magias, i: this.index })
+  }
+
+  fecharModal(){
+    console.log(this.magias);
+    console.log(this.magiasPai);
+    if (this.magias.length > 0)
+      if (confirm("Sair sem salvar?")) {
         this.magias = this.magiasPai;
+
+      }
     this.salvarAlteracoes.emit({ Magias: this.magias, i: this.index })
   }
 
   marcarMagias() {
-    for (let i = 0; i < this.magiasPai.length; i++)
-      for (let y = 0; i < this.magiasPai[i].length; y++)
-        for (let x = 0; i < this.inputs.length; x++) {
-          if (this.magiasPai[i].includes(this.inputs[x].value))
-            //this.inputs[x].elemento.checked = true;
-            alert('É NOIZ')
-        }
+    
   }
 
   constructor() { }
